2640a95674828828e9cfe67f96dc8ed2dc80a63e
Ihor <ihorrykun@gmail.com>
2023-03-08 12:46:51 +0200

    Update Filterds.jsx

--- src/components/Filter/Filterds.jsx
+++ src/components/Filter/Filterds.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import css from '../Filter/Filterds.module.css';
+import { useDispatch } from 'react-redux';
+import { isFilter } from 'Redux/phonebook/phonebookSlice';
 
-export const Filterds = ({filter, onInput}) => {
+export const Filterds = () => {
+  const dispatch = useDispatch();
+
+  const hendleFilter = e => {
+    dispatch(isFilter(e.target.value));
+  };
   return (
     <div>
       <p>Find contacts by name</p>
-      <input
-        className={css.input}
-        name="filter"
-        value={filter}
-        onChange={e => onInput(e)}
-      />
+      <input className={css.input} type="text" onChange={hendleFilter} />
     </div>
   );
 };
